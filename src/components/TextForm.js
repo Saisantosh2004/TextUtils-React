1bbc381656aa6211002644f7030be1d09f58ceac
Bonagiri sai santosh <bonagirisaisantosh2004@gmail.com>
2023-04-06 15:23:57 +0530

    video 8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,9 +19,20 @@ export default function TextForm(props) {
         setText(upText);
     }
     
+    const handleOnClearClick=()=>{
+        setText("")  
+    }
+
+    const handleOnRandomClick=()=>{
+        let loremContent='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci ac auctor augue mauris augue neque gravida in fermentum. Sed viverra tellus in hac habitasse. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Sit amet consectetur adipiscing elit ut aliquam purus sit. Natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Interdum velit euismod in pellentesque massa placerat. Scelerisque fermentum dui faucibus in ornare quam viverra. Pretium viverra suspendisse potenti nullam ac tortor. Sem viverra aliquet eget sit amet. Elit ut aliquam purus sit amet luctus venenatis lectus. Suspendisse ultrices gravida dictum fusce ut placerat'  
+
+        setText(loremContent);
+    }
+
     const handleOnChange=(event)=>{
         setText(event.target.value)  
     }
+
 
     return (
         <>
@@ -31,7 +42,9 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control my-3 TextForm-bg" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
                     <button className='btn btn-primary mx-1' onClick={handleOnUpClick}>Convert to UpperCase</button>
-                    <button className='btn btn-primary mx-1' onClick={handleOnLoClick}>Convert to Lowercase</button>
+                    <button className='btn btn-warning mx-1' onClick={handleOnLoClick}>Convert to Lowercase</button>
+                    <button className='btn btn-danger mx-1' onClick={handleOnClearClick}>Clear Text</button>
+                    <button className='btn btn-success mx-1' onClick={handleOnRandomClick}>Random Text</button>
                 </div>
             </div>
         </div>
