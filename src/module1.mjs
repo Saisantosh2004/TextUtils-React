bd17487b2e497f9ddc8847f428b9cd3694bedaeb
Bonagiri sai santosh <bonagirisaisantosh2004@gmail.com>
2023-04-06 14:00:23 +0530

    video 6

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+import def,{a,c,d} from './module2.mjs'
+
+console.log(def)
+console.log(a)
+console.log(c)
+console.log(d)
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a="harry";
+const b="santosh";
+const c="karthik";
+const d="praneeth";
+
+export default b;
+export {a};
+export {c};
+export {d};
