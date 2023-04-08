0191e8c01750b9f21709ba6c0349dcfb9a63e0d2
Bonagiri sai santosh <bonagirisaisantosh2004@gmail.com>
2023-04-08 21:58:23 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 
+import { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+
+  const showAlert=(msg,type)=>{
+    setAlert({
+      msg:msg,
+      type:type
+    })
+
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor= '#4f5153';
+      showAlert('Dark Mode Enabled',"success")
+      // document.title="TextUtils -DarkMode"
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert('Light Mode Enabled',"success")
+    }
+  }
+
   return (
     <>
-    <NavBar title="TextUtils" aboutText="About"/>
-    <TextForm heading="Enter the text below to analyze"/>
+    
+    {/* <Router> */}
+      <NavBar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <TextForm heading="Enter the text :" mode={mode} showAlert={showAlert}/>
+      {/* <Routes>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/"  element={<TextForm heading="Enter the text :" mode={mode} showAlert={showAlert}/>}></Route>
+      </Routes>
+    </Router> */}
+    
+    
     </>
     
   );
